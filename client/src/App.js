5a44adbe4158fb90b5aa767263002459c2fb2841
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2024-07-06 20:02:29 +0530

    setup

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <Routes>
+        <Route path="" element={<div> hiiiii</div>} />
+       
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
